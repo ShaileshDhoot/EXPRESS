484257ff936c6688d3b8faa3fcb6231ef3dd893b
ShaileshDhoot <shaileshdhoot25@gmail.com>
2023-04-10 03:41:11 +0530

    middlewares and parser

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const http = require('http')
 
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req,res,next)=>{
-    console.log('middleware')
-    next()
+app.use(bodyParser.urlencoded({extended: false}))
+// app.use(bodyParser.json())
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="quantity"><button type="submit">Add Product</button></form>')   
 })
 
-app.use((req,res,next)=>{
-    console.log('second middleware')
-    res.send('<h1> hello to node js </h1>')
-    // res.send({name: 'shailesh'})
+// app.use('/product',( req,res,next)=>{            // having issue with output in console--->[Object: null prototype]
+//     console.log(req.body);
+//     res.redirect('/');
+// })
+// app.get('/product',( req,res,next)=>{  //only get the /product page
+//     console.log(req.body);
+//     res.redirect('/');
+// })
+app.post('/product',( req,res,next)=>{      // works same as use method , but if we go to /product , it wont redirect
+    console.log(req.body);
+    res.redirect('/');
 })
 
-// const server = http.createServer(app)
-
-// server.listen(3000)
+app.use('/',(req,res,next)=>{
+    res.send('<h1> Hello to Express </h1>')   
+})
 
 app.listen(3000)
